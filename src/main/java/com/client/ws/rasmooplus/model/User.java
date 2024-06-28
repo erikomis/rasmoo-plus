68363cfb08c92469e624de1d2308bb04cb58110c
erik-martins <erik.ozzetti.martins@gmail.com>
2024-06-28 20:02:52 -0300

    fix: @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/com/client/ws/rasmooplus/model/User.java
+++ src/main/java/com/client/ws/rasmooplus/model/User.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(
             name = "users_id"
     )
@@ -37,7 +37,7 @@ public class User implements Serializable {
 
     @Column(name = "dt_subscription"
     )
-    private LocalDate dtSubscription = LocalDate.now();
+    private LocalDate dtSubscription;
 
     @Column(
             name = "dt_expiration"
@@ -57,75 +57,5 @@ public class User implements Serializable {
     )
     private SubscriptionType subscriptionType;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public LocalDate getDtSubscription() {
-        return dtSubscription;
-    }
-
-    public void setDtSubscription(LocalDate dtSubscription) {
-        this.dtSubscription = dtSubscription;
-    }
-
-    public LocalDate getDtExpired() {
-        return dtExpired;
-    }
-
-    public void setDtExpired(LocalDate dtExpired) {
-        this.dtExpired = dtExpired;
-    }
-
-    public SubscriptionType getSubscriptionType() {
-        return subscriptionType;
-    }
-
-    public void setSubscriptionType(SubscriptionType subscriptionType) {
-        this.subscriptionType = subscriptionType;
-    }
-
-    public UserType getUserType() {
-        return userType;
-    }
-
-    public void setUserType(UserType userType) {
-        this.userType = userType;
-    }
 }
--- src/main/java/com/client/ws/rasmooplus/model/UserType.java
+++ src/main/java/com/client/ws/rasmooplus/model/UserType.java
@@ -18,7 +18,7 @@ public class UserType implements Serializable {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(
             name = "user_type_id"
     )
