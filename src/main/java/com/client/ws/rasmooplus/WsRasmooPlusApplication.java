bf85c5ee663bb2e27bcbe6ce86ae00f4fb046f0f
erik-martins <erik.ozzetti.martins@gmail.com>
2024-06-28 20:03:05 -0300

    fet:EnableWebMvc

--- src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
+++ src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -1,9 +1,10 @@
 package com.client.ws.rasmooplus;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class WsRasmooPlusApplication {
 
     public static void main(String[] args) {
