6cb8ac3cb6375b3c6a283c1af98865b407d45229
erik-martins <erik.ozzetti.martins@gmail.com>
2024-06-28 02:24:06 -0300

    feat: validation

--- src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
@@ -1,7 +1,12 @@
 package com.client.ws.rasmooplus.dto;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,12 +15,20 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class SubscriptionTypeDto {
 
     private Long id;
+
+    @NotBlank(message = "O campo name não pode ser nulo ou vazio")
     private String name;
+    @Max(value = 12, message = "campo accessMonth não pode ser maior que 12")
     private Long accessMonth;
+    @NotNull(message = "O campo price não pode ser nulo")
     private BigDecimal price;
+
+    @NotBlank(message = "O campo productKey não pode ser nulo ou vazio")
+    @Size( min = 5, max=12 , message = "O campo productKey precisa ter entre 5 a 15 caracteres" )
     private String  productKey;
 
 
