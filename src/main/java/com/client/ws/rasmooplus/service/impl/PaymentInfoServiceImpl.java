433f28bf19d1278003727833e5639347318dc220
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-18 15:51:08 -0300

    feat: controller  PaymentInfoController PaymentInfoService PaymentInfoServiceImpl

---
+++ src/main/java/com/client/ws/rasmooplus/service/impl/PaymentInfoServiceImpl.java
@@ -0,0 +1,81 @@
+package com.client.ws.rasmooplus.service.impl;
+
+import com.client.ws.rasmooplus.dto.PaymentProcessDto;
+import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
+import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
+import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
+import com.client.ws.rasmooplus.exception.BusinessExcepion;
+import com.client.ws.rasmooplus.exception.NotFoudException;
+import com.client.ws.rasmooplus.integration.MailIntegration;
+import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
+import com.client.ws.rasmooplus.mapper.UserPaymentInfoMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.CreditCardMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.CustomerMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.OrderMapper;
+import com.client.ws.rasmooplus.mapper.wsraspay.PaymentMapper;
+import com.client.ws.rasmooplus.model.User;
+import com.client.ws.rasmooplus.model.UserPaymentInfo;
+import com.client.ws.rasmooplus.repository.UserPaymentInfoRepository;
+import com.client.ws.rasmooplus.repository.UserRepository;
+import com.client.ws.rasmooplus.service.PaymentInfoService;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class PaymentInfoServiceImpl  implements PaymentInfoService {
+
+    private final UserRepository userRepository;
+    private final UserPaymentInfoRepository userPaymentInfoRepository;
+    private final WsRaspayIntegration wsRaspayIntegration;
+    private final MailIntegration mailIntegration;
+
+    PaymentInfoServiceImpl(UserRepository userRepository, UserPaymentInfoRepository userPaymentInfoRepository,
+                           WsRaspayIntegration wsRaspayIntegration, MailIntegration mailIntegration){
+        this.userRepository = userRepository;
+        this.userPaymentInfoRepository = userPaymentInfoRepository;
+        this.wsRaspayIntegration = wsRaspayIntegration;
+        this.mailIntegration = mailIntegration;
+    }
+
+
+
+    @Override
+    public Boolean process(PaymentProcessDto dto) {
+
+        var userOpt = userRepository.findById(dto.getUserPaymentInfoDto().getUserId());
+
+        if (userOpt.isEmpty()) {
+            throw new NotFoudException("User not found");
+        }
+
+        User user = userOpt.get();
+
+        if (Objects.nonNull(user.getSubscriptionType())) {
+            throw  new BusinessExcepion("Payment already processed");
+        }
+
+       CustomerDto customerDto=  wsRaspayIntegration.createCustomer(CustomerMapper.build(user));
+
+
+        OrderDto orderDto = wsRaspayIntegration.createOrder(OrderMapper.build(customerDto.getId(), dto));
+
+
+        PaymentDto paymentDto =  PaymentMapper.build(customerDto.getId(), orderDto.getId(), CreditCardMapper.build(dto.getUserPaymentInfoDto(), user.getCpf()));
+
+
+
+        Boolean sucessPayment = wsRaspayIntegration.processPayment(paymentDto);
+
+        if (Boolean.TRUE.equals(sucessPayment)) {
+            UserPaymentInfo userPaymentInfo = UserPaymentInfoMapper.fromDtoToEntity(dto.getUserPaymentInfoDto(), user);
+            userPaymentInfoRepository.save(userPaymentInfo);
+            mailIntegration.send(user.getEmail(),"Usuario: "+ user.getEmail()+" - Senha: alunorasmoo","Acesso liberado");
+            return true;
+        }
+
+
+        return false;
+    }
+}
