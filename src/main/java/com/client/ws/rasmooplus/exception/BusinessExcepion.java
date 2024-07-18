ec4d7f2bf72e28614a5b129739ed4cec87df91d0
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-18 15:40:21 -0300

    feat: BusinessExcepion

---
+++ src/main/java/com/client/ws/rasmooplus/exception/BusinessExcepion.java
@@ -0,0 +1,8 @@
+package com.client.ws.rasmooplus.exception;
+
+public class BusinessExcepion extends RuntimeException {
+
+    public BusinessExcepion(String message) {
+        super(message);
+    }
+}
