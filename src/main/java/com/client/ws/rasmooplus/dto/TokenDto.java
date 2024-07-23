fce9c0801d3cd74b76d05da473201706406d88b6
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-23 14:25:14 -0300

    feat: TokenService

---
+++ src/main/java/com/client/ws/rasmooplus/dto/TokenDto.java
@@ -0,0 +1,18 @@
+package com.client.ws.rasmooplus.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TokenDto {
+
+    private  String token;
+
+
+    private String type;
+}
---
+++ src/main/java/com/client/ws/rasmooplus/service/TokenService.java
@@ -0,0 +1,9 @@
+package com.client.ws.rasmooplus.service;
+
+public interface TokenService {
+    String getToken(Long userId);
+
+    Boolean isValid(String token);
+
+    Long getUserId(String token);
+}
