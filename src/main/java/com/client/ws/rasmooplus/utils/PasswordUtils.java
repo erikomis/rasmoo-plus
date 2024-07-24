e628d8119a269fa5669aa5e67e314a110b513830
erik-martins <erik.ozzetti.martins@gmail.com>
2024-07-24 14:30:51 -0300

    feat:  utils PasswordUtils

---
+++ src/main/java/com/client/ws/rasmooplus/utils/PasswordUtils.java
@@ -0,0 +1,21 @@
+package com.client.ws.rasmooplus.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordUtils {
+
+
+    private  PasswordUtils() {
+    }
+
+
+    public static String encode(String password) {
+        return new BCryptPasswordEncoder().encode(password);
+    }
+
+
+    public static boolean matches(CharSequence password, String encodedPassword) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.matches(password, encodedPassword);
+    }
+}
